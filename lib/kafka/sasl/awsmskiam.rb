33b5477b5f8a4e3f7f6732976fdaa73d6477c08d
Andrew Garrett <garrett528@gmail.com>
2021-11-26 13:57:05 -0500

    Update timestamp calculations

--- lib/kafka/sasl/awsmskiam.rb
+++ lib/kafka/sasl/awsmskiam.rb
@@ -31,7 +31,14 @@ module Kafka
 
         host_without_port = host.split(':', -1).first
 
-        msg = authentication_payload(host: host_without_port)
+        ymd = Time.now.utc.strftime("%Y%m%d")
+        hms = Time.now.utc.strftime("%H%M%S")
+        time_now = "#{ymd}T#{hms}Z"
+
+        # ymd = "20211125"
+        # time_now = "20211125T141421Z"
+
+        msg = authentication_payload(host: host_without_port, ymd: ymd, time_now: time_now)
         @logger.debug "Sending first client SASL AWS_MSK_IAM message:"
         @logger.debug msg
         encoder.write_bytes(msg)
@@ -58,38 +65,36 @@ module Kafka
         @digest ||= OpenSSL::Digest::SHA256.new
       end
 
-      def authentication_payload(host:)
-        now = Time.now
+      def authentication_payload(host:, ymd:, time_now:)
         {
           'version': "2020_10_22",
           'host': host,
           'user-agent': "ruby-kafka",
           'action': "kafka-cluster:Connect",
           'x-amz-algorithm': "AWS4-HMAC-SHA256",
-          'x-amz-credential': @access_key_id + "/" + now.strftime("%Y%m%d") + "/" + @aws_region + "/kafka-cluster/aws4_request",
-          'x-amz-date': now.strftime("%Y%m%dT%H%M%SZ"),
+          'x-amz-credential': @access_key_id + "/" + ymd + "/" + @aws_region + "/kafka-cluster/aws4_request",
+          'x-amz-date': time_now,
           'x-amz-signedheaders': "host",
           'x-amz-expires': "900",
-          'x-amz-signature': signature(host: host)
+          'x-amz-signature': signature(host: host, ymd: ymd, time_now: time_now)
         }.to_json
       end
 
-      def canonical_request(host:)
+      def canonical_request(host:, ymd:, time_now:)
         "GET\n" +
         "/\n" +
-        canonical_query_string + "\n" +
+        canonical_query_string(ymd: ymd, time_now: time_now) + "\n" +
         canonical_headers(host: host) + "\n" +
         signed_headers + "\n" +
         hashed_payload
       end
 
-      def canonical_query_string
-        now = Time.now
+      def canonical_query_string(ymd:, time_now:)
         URI.encode_www_form(
           "Action" => "kafka-cluster:Connect",
           "X-Amz-Algorithm" => "AWS4-HMAC-SHA256",
-          "X-Amz-Credential" => @access_key_id + "/" + now.strftime("%Y%m%d") + "/" + @aws_region + "/kafka-cluster/aws4_request",
-          "X-Amz-Date" => now.strftime("%Y%m%dT%H%M%SZ"),
+          "X-Amz-Credential" => @access_key_id + "/" + ymd + "/" + @aws_region + "/kafka-cluster/aws4_request",
+          "X-Amz-Date" => time_now,
           "X-Amz-Expires" => "900",
           "X-Amz-SignedHeaders" => "host"
         )
@@ -107,22 +112,19 @@ module Kafka
         bin_to_hex(digest.digest(""))
       end
 
-      def string_to_sign(host:)
-        now = Time.now
+      def string_to_sign(host:, ymd:, time_now:)
         "AWS4-HMAC-SHA256" + "\n" +
-        now.strftime("%Y%m%dT%H%M%SZ") + "\n" +
-        now.strftime("%Y%m%d") + "/" + @aws_region + "/kafka-cluster/aws4_request" + "\n" +
-        bin_to_hex(digest.digest(canonical_request(host: host)))
+        time_now + "\n" +
+        ymd + "/" + @aws_region + "/kafka-cluster/aws4_request" + "\n" +
+        bin_to_hex(digest.digest(canonical_request(host: host, ymd: ymd, time_now: time_now)))
       end
 
-      def signature(host:)
-        now = Time.now
-
-        date_key = OpenSSL::HMAC.digest("SHA256", "AWS4" + @secret_key_id, now.strftime("%Y%m%d"))
+      def signature(host:, ymd:, time_now:)
+        date_key = OpenSSL::HMAC.digest("SHA256", "AWS4" + @secret_key_id, ymd)
         date_region_key = OpenSSL::HMAC.digest("SHA256", date_key, @aws_region)
         date_region_service_key = OpenSSL::HMAC.digest("SHA256", date_region_key, "kafka-cluster")
         signing_key = OpenSSL::HMAC.digest("SHA256", date_region_service_key, "aws4_request")
-        signature = bin_to_hex(OpenSSL::HMAC.digest("SHA256", signing_key, string_to_sign(host: host)))
+        signature = bin_to_hex(OpenSSL::HMAC.digest("SHA256", signing_key, string_to_sign(host: host, ymd: ymd, time_now: time_now)))
 
         signature
       end
